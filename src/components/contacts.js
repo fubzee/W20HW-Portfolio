6c1f8e12316ee3ea3035f56c2dbd5bdee362c152
Sharon <fubzee@gmail.com>
2022-04-29 00:38:27 +1000

    Added pdf

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -12,19 +12,16 @@ export default function Contact() {
         <br></br>
         <div className ="row"> 
             <div className = "col-md-3" id="ContactDetails">
-               {/* <p span ><img id="phone" src="/images/phone.jpg" alt="phone"></img>  +61-412692585</p>               */}
                 <p className="credits"><i className='bi bi-phone'></i> +61-412692585</p>
             </div>
             <div className = "col-md-3">
-                {/* <a img id="email" src="/images/email.jpg" href='mailto: fubzee@gmail.com?subject="Development Portfolio"'>   fubzee@gmail.com</a>     */}
                 <p><i className='bi bi-envelope'></i><a href='mailto: fubzee@gmail.com?subject="Development Portfolio"'>   fubzee@gmail.com</a></p>
             </div>
             <div className = "col-md-3">
-                {/* <a href="https://www.linkedin.com/in/sharon-martin-itexp/"><img id="linkedin" src="/images/linkedin.jpg" alt="LinkedIn"></img>  Sharon Martin</a> */}
                 <p><i className='bi bi-linkedin'></i><a href="https://www.linkedin.com/in/sharon-martin-itexp/">  Sharon Martin</a></p>
             </div>
             <div className = "col-md-3">
-                {/* <a href="SM-FSD.pdf" download><img src="/images/resume.png" alt="Resume"></img> Resume</a> */}
+                <a href="SM-FSD.pdf" download><img src="./resume.png" alt="Resume"></img> Resume</a>
                 <p><i className="bi bi-file-arrow-down"></i><a href="SM-FSD.pdf" download> Resume</a></p>
             </div>
         </div>
